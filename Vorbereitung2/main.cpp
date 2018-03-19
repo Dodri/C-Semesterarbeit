66c5f789f1adc7ef942f204152f289ad4934487b
Dodri <32789926+Dodri@users.noreply.github.com>
2018-03-19 16:37:39 +0100

    Stufe 1&2 vorläufig

--- Vorbereitung2/main.cpp
+++ Vorbereitung2/main.cpp
@@ -1,6 +1,7 @@
-/*#include <iostream>
-#include <string>
+#include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -8,28 +9,291 @@ void printHeader(int sum);
 char getCommand();
 void doCommand(char command);
 
-void convertData(string source1, string source2);
-void search(string keyword);
-void help();
 
-int main() {
-    int sum = 0;
+struct Platte {
+    string Platte;
+    string id;
+    string Albumtitel;
+    string Kuenstler;
+    string Ort;
+    string Plattenladen;
+    int Bestand;
+    float Preis;
+};
+
+struct txt {
+    int id;
+    int Bestand;
+    float Preis;
+};
+
+Platte plattenArr[10];
+
+void zuweisen(string tagname, string taginhalt, int i){
+    if(tagname == "Albumtitel")
+    {
+        plattenArr[i].Albumtitel = taginhalt;
+        //platte->Albumtitel = taginhalt;
+      //break;
+    } else if(tagname == "id") {
+        plattenArr[i].id = taginhalt;
+    }
+
+}
+
+void parseTxt(){
+
+    ifstream txtin;
+    txtin.open("Musiker.txt");
+
+    string line;
+    int zaehler = 0;
+    int i;
+
+    while(getline(txtin, line)){
+        string array[4];
+        int arr_i = 0;
+
+        istringstream iss(line);
+        do {
+               string subs;
+               iss >> subs;
+               array[arr_i] = subs;
+               arr_i++;
+        } while (iss);
+        string id = array[0];
+        int Bestand = stoi(array[1], nullptr);
+        float Preis = stof(array[2]);
+        zaehler++;
+        for(i = 0; i < 10; i++) {
+            if(plattenArr[i].id.compare(id) == 0) {
+                plattenArr[i].Bestand = Bestand;
+                plattenArr[i].Preis = Preis;
+            }
+        }
+    }
+
+
+}
+
+void konvertieren(){
+
+    ofstream outputFile;
+    outputFile.open("Output.xml");
+
+    outputFile << "<Plattenliste>" << endl;
+
+    int i = 0;
+    for(i = 0; i < 10; i++){
+        outputFile << "<Platte xml:id=\""+ plattenArr[i].id + "\">" << endl;
+        outputFile << "\t<Albumtitel>" + plattenArr[i].Albumtitel + "</Albumtitel>" << endl;
+        outputFile << "\t<Bestand>" + to_string(plattenArr[i].Bestand) + "</Bestand>" << endl;
+        outputFile << "</Platte>" << endl;
+    }
+
+    outputFile << "</Plattenliste>" << endl;
+
+    outputFile.close();
+
+}
+
+void ausgabe(int index) {
+    cout << "ID: " + plattenArr[index].id << endl;
+    cout << "ALbumtitel: " + plattenArr[index].Albumtitel << endl;
+    cout << "Bestand: " + to_string(plattenArr[index].Bestand) << endl;
+}
+
+void search(){
+    string Category;
+    string searchFor;
+
+    cout << "Category: ";
+    cin >> Category;
+    cout << "Search for: ";
+    cin >> searchFor;
+
+    string cat = Category; //transform(Category.begin(), Category.end(), Category.begin(), tolower);
+
+    bool foundSth = false;
+    string Attribut;
+
+    for(int i = 0; i < 10; i++){
+        if(
+            (cat.find('id') && plattenArr[i].id.find(searchFor) != string::npos)
+            || (cat.find("Albumtitel") && plattenArr[i].Albumtitel.find(searchFor) != string::npos)
+            || (cat.find("Bestand") && to_string(plattenArr[i].Bestand).find(searchFor) != string::npos)
+        ) {
+            ausgabe(i);
+            foundSth = true;
+        }
+    }
+
+    if(!foundSth) {
+        cout << "Keine Platten gefunden" << endl;
+    }
+}
+
+void parseXML(){
+    char zeichen;
+    char puffer[100];
+    int zaehler;
+    int zustand;
+    string tagname;
+    char taginhalt[666];
+    int tagzaehler = 0;
+    int i = 0;
+
+    /* Dabei möge gelten:
+       zustand = 0 : Wir sind "zwischen den Tags".
+       zustand = 1 : Wir bearbeiten einen Tagnamen.
+       zustand = 2 : Wir erwarten einen Attributnamen.
+       zustand = 3 : Wir erwarten einen Attributwert.
+       zustand = 4 : Wir verarbeiten einen Attributwert. */
+
+    ifstream eingabe;
+
+    eingabe.open("Musiker.xml");
+
+    Platte platte; //= new Platte();
+    //platte =(struct Platte*) malloc(sizeof(Platte));
+
+
+    bool isid = false;
+    bool isplatte = false;
+    bool isalbumtitel = false;
+
+
+    for (zaehler=0,zustand=0;;)
+        {
+        eingabe.get(zeichen);
+        if (eingabe.eof()) break;
+
+       switch(zeichen)
+          {
+       case '<':
+          taginhalt[tagzaehler]='\0';
+
+          zuweisen(tagname, taginhalt, i);
+
+          if(tagname == "/Platte"){
+              //plattenarray[i] = platte;
+              //memcpy(&plattenarray, &platte, sizeof(platte) * i);
+              //*(plattenArr + i) = platte;
+              //plattenArr[i] = platte;
+              i++;
+              platte = {};
+             // platte = (struct Platte*) malloc(sizeof(Platte));
+             // platte = new Platte();
+          }
+
+          tagzaehler = 0;
+          zustand=1;
+          zaehler=0;
+          break;
+
+       case '>':
+          puffer[zaehler] = '\0';
+          tagname = puffer;
+          zustand=0;
+          break;
+
+       case ' ':
+
+          if (zustand == 1)
+             {
+             zustand = 2;
+             puffer[zaehler] = '\0';
+             zaehler=0;
+             }
+          else if (zustand == 4)
+             {
+             puffer[zaehler] = zeichen;
+             zaehler++;
+             }
+          else if (zustand == 0){
+              taginhalt[tagzaehler] = ' ';
+              tagzaehler++;
+          }
+          break;
+
+       case '=':
+          if (zustand == 2)
+             {
+             zustand = 3;
+             puffer[zaehler] = '\0';
+             string compare = puffer;
+             if(compare == "xml:id"){
+                 isid = true;
+             }
+             zaehler=0;
+             }
+          else if (zustand == 4)
+             {
+             puffer[zaehler] = zeichen;
+             zaehler++;
+
+             }
+          break;
+
+       case '"':
+          if (zustand == 3)
+             {
+             zustand = 4;
+             zaehler = 0;
+             }
+          else if (zustand == 4)
+             {
+             zustand = 2;
+             puffer[zaehler] = '\0';
+             if(isid) {
+                 string compare = puffer;
+                 zuweisen("id", compare, i);
+             }
+             zaehler=0;
+             }
+          break;
+
+       default:
+          if (zustand != 0)
+             {
+             puffer[zaehler] = zeichen;
+             zaehler++;
+             }
+          if(zustand == 0){
+            taginhalt[tagzaehler] = zeichen;
+            tagzaehler++;
+
+          }
+          break;
+          }
+       }
+        eingabe.close();
+}
+
+int Plattenanazahl(){
+    return(sizeof(plattenArr)/sizeof(*plattenArr));
+
+}
+
+int main()
+{
     char command;
 
-    printHeader(sum);
+    parseXML();
+    parseTxt();
+
+    printHeader(Plattenanazahl());
 
     do {
         command = getCommand();
         doCommand(command);
     } while(command != 'e');
 
-    return 0;
 }
 
 void printHeader(int sum) {
-    cout << "**** stocktaking cli 2018 ****" << endl;
-    cout << "******************************" << endl;
-    cout << "€: " << sum << endl;
+    cout << "Plattensammlung" << endl;
+    cout << "Platten " << sum << endl;
     cout << endl;
     cout << "(c)onvert data" << endl;
     cout << "(s)earch" << endl;
@@ -48,249 +312,21 @@ char getCommand() {
 void doCommand(char command) {
     switch(command) {
         case 'c':
-            convertData("", "");
+            konvertieren();
             break;
         case 's':
-            search("");
+        search();
+
             break;
         case 'h':
-            help();
+
             break;
         case 'e':
             cout << "Good bye.";
             break;
+        case 'p':
+
         default:
             cout << "Please insert an command from above or use 'h' for help" << endl;
     }
-}
-
-void convertData(string source1, string source2) {
-    cout << "convert data" << endl;
-}
-
-void search(string keyword) {
-    cout << "search" << endl;
-}
-
-void help() {
-    cout << "help" << endl;
-}
-*/
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <list>
-
-using namespace std;
-
-struct Platte {
-    string Platte;
-    string id;
-    string Albumtitel;
-    string Kuenstler;
-    string Ort;
-    string Plattenladen;
-    string Bestand;
-    string Preis;
-};
-
-struct txt {
-    int id;
-    int Bestand;
-    float Preis;
-};
-
-void zuweisen(string tagname, string taginhalt, Platte *platte){
-    if(tagname == "Albumtitel")
-    {
-        platte->Albumtitel = taginhalt;
-      //break;
-    }
-
-}
-
-void parseTxt(){
-
-    ifstream txtin;
-    ifstream txtin2;
-
-    txtin.open("Musiker.txt");
-
-    txtin2.open("Musiker.txt");
-
-    string line;
-    int zaehler = 0;
-
-    while(getline(txtin, line)){
-        zaehler++;
-    }
-
-    txt List[zaehler];
-
-    int zaehler2 = 0;
-
-    while(getline(txtin2, line)){
-
-        txt Item;
-        string array[4];
-        int i = 0;
-
-        istringstream iss(line);
-           do {
-               string subs;
-               iss >> subs;
-               array[i] = subs;
-               i++;
-           }
-        while (iss);
-
-        Item.id = stoi(array[0], nullptr);
-        Item.Bestand = stoi(array[1], nullptr);
-        Item.Preis = stof(array[2]);
-
-        List[zaehler2] = Item;
-        zaehler2++;
-    }
-
-int i;
-    for(i = 0; i < zaehler; i++){
-        cout << List[i].id << endl;
-    }
-    }
-
-
-
-int main()
-{
-char zeichen;
-char puffer[100];
-int zaehler;
-int zustand;
-string tagname;
-char taginhalt[666];
-int tagzaehler;
-
-/* Dabei möge gelten:
-   zustand = 0 : Wir sind "zwischen den Tags".
-   zustand = 1 : Wir bearbeiten einen Tagnamen.
-   zustand = 2 : Wir erwarten einen Attributnamen.
-   zustand = 3 : Wir erwarten einen Attributwert.
-   zustand = 4 : Wir verarbeiten einen Attributwert. */
-
-ifstream eingabe;
-
-eingabe.open("Musiker.xml");
-
-Platte platte;
-
-bool isid = false;
-bool isplatte = false;
-bool isalbumtitel = false;
-
-
-for (zaehler=0,zustand=0;;)
-    {
-    eingabe.get(zeichen);
-    if (eingabe.eof()) break;
-
-   switch(zeichen)
-      {
-   case '<':
-      taginhalt[tagzaehler]='\0';
-
-      zuweisen(tagname, taginhalt, &platte);
-
-      cout << taginhalt << endl;
-      tagzaehler = 0;
-      zustand=1;
-      zaehler=0;
-      break;
-
-   case '>':
-      puffer[zaehler] = '\0';
-      tagname = puffer;
-      zustand=0;
-      break;
-
-   case ' ':
-// Wenn wir bisher einen Tagnamen gelesen haben, endet er.
-      if (zustand == 1)
-         {
-         zustand = 2;
-         puffer[zaehler] = '\0';
-
-         cout << puffer << endl;
-         zaehler=0;
-         }
-      else if (zustand == 4)
-         {
-         puffer[zaehler] = zeichen;
-         zaehler++;
-         }
-      else if (zustand == 0){
-          taginhalt[tagzaehler] = ' ';
-          tagzaehler++;
-      }
-      break;
-
-   case '=':
-      if (zustand == 2)
-         {
-         zustand = 3;
-         puffer[zaehler] = '\0';
-         string compare = puffer;
-         if(compare == "xml:id"){
-             isid = true;
-         }
-         cout << puffer << endl;
-         zaehler=0;
-         }
-      else if (zustand == 4)
-         {
-         puffer[zaehler] = zeichen;
-         zaehler++;
-
-         }
-      else cout << "Fehlerhaftes Zeichen! '='" << endl;
-      break;
-
-   case '"':
-      if (zustand == 3)
-         {
-         zustand = 4;
-         zaehler = 0;
-         }
-      else if (zustand == 4)
-         {
-         zustand = 2;
-         puffer[zaehler] = '\0';
-         if(isid) {
-             string compare = puffer;
-             platte.id = compare;
-         }
-         cout << puffer << endl;
-         zaehler=0;
-         }
-      else cout << "Fehlerhaftes Zeichen! '\"'" << endl;
-      break;
-
-   default:
-      if (zustand != 0)
-         {
-         puffer[zaehler] = zeichen;
-         zaehler++;
-         }
-      if(zustand == 0){
-        taginhalt[tagzaehler] = zeichen;
-        tagzaehler++;
-
-      }
-      break;
-      }
-   }
-eingabe.close();
-
-parseTxt();
 }
